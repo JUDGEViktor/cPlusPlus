4b6b9918042a38473f4ff93144f89cb2a60c0fab
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-04-06 11:49:46 +0300

    Update Плюсы_2Лаба.h

--- Плюсы_2Лаба.h
+++ Плюсы_2Лаба.h
@@ -9,7 +9,7 @@ using namespace std;
 #define HEIGHT 20
 #define NUMBER_TRACK 10
 
-enum collision {
+enum collisions {
 	upper_collision,
 	bottom_collision,
 	right_collision,
@@ -27,13 +27,13 @@ private:
 
 	coordinates COORD;
 
+public:
+	
 	Vector& operator += (Vector& NewWay) {
 		COORD.x += NewWay.COORD.x;
 		COORD.y += NewWay.COORD.y;
 		return *this;
 	}
-
-public:
 
 	void ChangeX(float X) {
 		COORD.x = X;
@@ -43,7 +43,9 @@ public:
 		COORD.y = Y;
 	}
 
-	friend class Mob;
+	coordinates GetCOORD() {
+		return COORD;
+	}
 };
 
 class Mob {
@@ -54,15 +56,15 @@ private:
 
 	coordinates COORD;
 
-	collision Collisions(RECT rcClient) {
+	collisions collisions_result(RECT rcClient) {
 
-		if (COORD.x + HEIGHT + Way.COORD.x > rcClient.right)
+		if (COORD.x + HEIGHT + Way.GetCOORD().x > rcClient.right)
 			return right_collision;
-		else if (COORD.x + Way.COORD.x < rcClient.left)
+		else if (COORD.x + Way.GetCOORD().x < rcClient.left)
 			return left_collision;
-		else if (COORD.y - HEIGHT + Way.COORD.y < rcClient.top)
+		else if (COORD.y - HEIGHT + Way.GetCOORD().y < rcClient.top)
 			return bottom_collision;
-		else if (COORD.y + Way.COORD.y > rcClient.bottom)
+		else if (COORD.y + Way.GetCOORD().y > rcClient.bottom)
 			return upper_collision;
 		else
 			return no_collision;
@@ -76,28 +78,28 @@ public:
 	
 	void Move(RECT rcClient, Vector &NewWay) {
 
-		switch (Collisions(rcClient)) {
+		switch (collisions_result(rcClient)) {
 
 		case right_collision:
-			Way.ChangeX(-Way.COORD.x);
+			Way.ChangeX(-Way.GetCOORD().x);
 			
 			break;
 
 		case left_collision:
-			Way.ChangeX(-Way.COORD.x);
+			Way.ChangeX(-Way.GetCOORD().x);
 			break;
 
 		case upper_collision:
-			Way.ChangeY(-Way.COORD.y);
+			Way.ChangeY(-Way.GetCOORD().y);
 			break;
 
 		case bottom_collision:
-			Way.ChangeY(-Way.COORD.y);
+			Way.ChangeY(-Way.GetCOORD().y);
 			break;
 
 		case no_collision:
-			COORD.x += Way.COORD.x;
-			COORD.y += Way.COORD.y;
+			COORD.x += Way.GetCOORD().x;
+			COORD.y += Way.GetCOORD().y;
 			break;
 		}
 	}
