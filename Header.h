ba69ed9d6608a645f643c50dacf22a716d8a1b7b
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-04-30 22:15:37 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -12,34 +12,9 @@
 #include<memory>
 using namespace std;
 
-
-
-enum status {
-	NON,
-	FRIGHTENED,
-	//....//
-};
-
-
 struct coordinates {
 	int x;
 	int y;
 };
 
-
-enum directions {
-	NORTH, 
-	SOUTH, 
-	WEST, 
-	EAST
-};
-
-
-enum bonuses {
-	REGULAR_POINT = 10,    //Обычная точка
-	ENERGIZER = 50, 
-	FRUIT[] = {},
-	GHOST[] = {200, 400, 800, 1600},
-
-};
 
