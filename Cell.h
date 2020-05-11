b83ede4747a280d00702d402b5e8d39316f534cb
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-11 18:03:25 +0300

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -4,12 +4,8 @@
 enum bonuses {
 	REGULAR_POINT,    //Обычная точка
 	ENERGIZER,
-	FRUIT
-};
-
-enum status {
-	FREE, 
-	NOT_FREE
+	FRUIT,
+	NONE
 };
 
 //Класс клетки, хранит информацию о "проходимости" клетки, и какой бонус на ней лежит
@@ -18,19 +14,30 @@ class Cell {
 
 private:
 
-	status STATUS;
+	bool FREE;
 
 	//Лежит ли на этой клетке какой-то бонус и какой бонус
 	bonuses BONUS;
 
+	coordinates COORD;
+
 
 public:
-	
-	Cell(status setStatus, bonuses setBonus) {
-		STATUS = setStatus;
+
+	Cell(bool setStatus, bonuses setBonus, coordinates setCoordinates) {
+		FREE = setStatus;
 		BONUS = setBonus;
+
+		COORD.first = setCoordinates.first;
+		COORD.second = setCoordinates.second;
+	}
+
+	bool IsFreee() {
+		return FREE;
+	}
+
+	bonuses WhatBonus() {
+		return BONUS;
 	}
 
 };
-
-
