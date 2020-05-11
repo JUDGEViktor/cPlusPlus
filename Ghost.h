0b8d3d64d5cfbf9c3799472c225027125a575add
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-11 18:13:02 +0300

    Update Ghost.h

--- Ghost.h
+++ Ghost.h
@@ -1,23 +1,63 @@
 #pragma once
 #include "Header.h"
 #include "GameMob.h"
+#include "PacMan.h"
+#include "Cell.h"
+
 
 
 class Ghost : public GameMob {
 
 private:
 
+	float ManhattanDistance(coordinates src, coordinates dst) {
+		return abs(src.first - dst.first) + abs(src.second - dst.second);
+	}
 
 public:
 
-	//ф-ция, реализующая алгоритм A* для поиска пакмена
-	//Искать пакмена приведения будут не больше 15 клеток
-	//Если пакмена находится от приведения на расстоянии больше чем 2 клетки и приведение искало пакмена уже 15 клеток
-	//то приведение начинает искать случайную точку в лабиринте 5 ходов
-	vector<coordinates> FindWay() {}
+	Ghost() : GameMob(make_pair(15.0 * SQUARE_SIZE, 15.0 * SQUARE_SIZE)) {}
 
-	Ghost() : GameMob() {}
+	coordinates FindPacMan(vector<vector<Cell>> Maze, PacMan pacMan) {
 
-	void SpawnGhost() {}
+		//Check north
+		if (ManhattanDistance(make_pair(GetCOORD().first, GetCOORD().second - 1), pacMan.GetCOORD()) < ManhattanDistance(GetCOORD(), pacMan.GetCOORD())
+			&& Maze[GetCOORD().first][GetCOORD().second - 1].IsFreee())
+			return make_pair(GetCOORD().first, GetCOORD().second - 1);
+
+		//Check south
+		else if (ManhattanDistance(make_pair(GetCOORD().first, GetCOORD().second + 1), pacMan.GetCOORD()) < ManhattanDistance(GetCOORD(), pacMan.GetCOORD())
+			&& Maze[GetCOORD().first][GetCOORD().second + 1].IsFreee())
+			return make_pair(GetCOORD().first, GetCOORD().second + 1);
+
+		//Check west
+		else if (ManhattanDistance(make_pair(GetCOORD().first - 1, GetCOORD().second), pacMan.GetCOORD()) < ManhattanDistance(GetCOORD(), pacMan.GetCOORD())
+			&& Maze[GetCOORD().first - 1][GetCOORD().second].IsFreee())
+			return make_pair(GetCOORD().first - 1, GetCOORD().second);
+
+		//Check east
+		else if (ManhattanDistance(make_pair(GetCOORD().first + 1, GetCOORD().second), pacMan.GetCOORD()) < ManhattanDistance(GetCOORD(), pacMan.GetCOORD())
+			&& Maze[GetCOORD().first + 1][GetCOORD().second].IsFreee())
+			return make_pair(GetCOORD().first + 1, GetCOORD().second);
+
+	}
+
+	bool EatPacman(PacMan pacman) {
+
+		if (GetCOORD() == pacman.GetCOORD()) {
+
+			if (GetStatus() == FRIGHTENED) {
+				return false;
+			}
+
+			if (GetStatus() == REGULAR) {
+				return true;
+			}
+
+
+		}
+
+	}
+
 
 };
