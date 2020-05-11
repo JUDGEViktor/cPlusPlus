1b291e5133acad9ba4d31f58daaee43bfc9549ca
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-11 18:14:58 +0300

    Update Level.h

--- Level.h
+++ Level.h
@@ -4,20 +4,101 @@
 #include "PacMan.h"
 #include "Ghost.h"
 
-// Class of levels. Include mass of cells = labyrinth
-// Methods: generate bonuses, I think that's all for now.
+//Основной класс
+
+#define NUM_GHOSTS 4
+
+enum GAME_STATUS {
+	BEGIN,
+	PLAY,
+	DEATH,
+	NEXT_LEVEL,
+	END
+};
+GAME_STATUS currGameStatus = BEGIN;
+
 
 class Level {
 
 private:
 
-	Cvector<vector<Cell>> Map;
+	vector<vector<Cell>> Maze;
+	float LENGTH;
+	float HEIGHT;
 
+	vector <Ghost> aliveGhosts;
+
+	queue <Ghost> deadGhosts;
+
+	PacMan pacman;
+
+	int pacmanLifes = 4;
+
+	int countOfEatenGhosts;
 
 public:
 
-	Level() {}
+	void InitLevel(ifstream& fileWithLevel) {
 
-	void GenerateBonuses() {}
+		string line;
+		int i, j;
+
+		for (i = 0; getline(fileWithLevel, line); i++) {
+			Maze.push_back(vector<Cell>());
+			for (j = 0; j < line.size(); j++) {
+				switch (line[j]) {
+				case int('#') :
+					Maze[i].emplace_back(false, NONE, make_pair(SQUARE_SIZE*j, SQUARE_SIZE*i));
+					break;
+
+				case int('.') :
+					Maze[i].emplace_back(true, REGULAR_POINT, make_pair(SQUARE_SIZE*j, SQUARE_SIZE*i));
+					break;
+
+				case int('$') :
+					Maze[i].emplace_back(true, ENERGIZER, make_pair(SQUARE_SIZE*j, SQUARE_SIZE*i));
+					break;
+
+				default:
+					Maze[i].emplace_back(true, NONE, make_pair(SQUARE_SIZE*j, SQUARE_SIZE*i));
+					break;
+
+				}
+			}
+		}
+
+		LENGTH = SQUARE_SIZE * j;
+		HEIGHT = SQUARE_SIZE * i;
+
+	}
+
+
+	void Play() {
+
+		switch (currGameStatus) {
+		case BEGIN:
+			pacman.SpawnPacMan();
+			for (int i = 0; i < NUM_GHOSTS; i++) {
+				aliveGhosts.emplace_back();
+			}
+			currGameStatus = PLAY;
+			break;
+
+		case PLAY:
+			break;
+
+		case DEATH:
+			break;
+
+		case NEXT_LEVEL:
+			break;
+
+		case END:
+			break;
+
+		}
+
+
+	}
 
 };
