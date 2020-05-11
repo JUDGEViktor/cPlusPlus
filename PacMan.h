21e618c61134da3d7f95fb0d235043e259cf863e
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-11 18:15:43 +0300

    Update PacMan.h

--- PacMan.h
+++ PacMan.h
@@ -1,6 +1,9 @@
 #pragma once
+
+#include "resource.h"
 #include "Header.h"
 #include "GameMob.h"
+#include "Cell.h"
 
 enum directions {
 	NORTH,
@@ -13,22 +16,58 @@ class PacMan : public GameMob {
 
 private:
 
-	directions DIRECT;
-
-	void ChangeDirection(){}
-
-	bool TestStep(){}
-
-	void Move(){}
+	directions DIRECTION;
 
 public:
 
-	PacMan() : GameMob() {}
+	void SpawnPacMan() {
+		GameMob(make_pair(2.0 * SQUARE_SIZE, 7.0 * SQUARE_SIZE));
+	}
 
-	void ActionWithGhost() {}
+	void ChangeDirection(directions newDirection) {
+		DIRECTION = newDirection;
+	}
 
-	void ActinoWithBonuses() {}
+	bool TestStep(vector<vector<Cell>> Maze) {
 
-	void SpawnPacMan() {}
+		switch (DIRECTION) {
+		case NORTH:
+			return Maze[GetCOORD().first][GetCOORD().second - 1].IsFreee();
+			break;
+		case SOUTH:
+			return Maze[GetCOORD().first][GetCOORD().second + 1].IsFreee();
+			break;
+		case WEST:
+			return Maze[GetCOORD().first - 1][GetCOORD().second].IsFreee();
+			break;
+		case EAST:
+			return Maze[GetCOORD().first + 1][GetCOORD().second].IsFreee();
+			break;
+		}
+
+
+	}
+
+	void Move(vector<vector<Cell>> Maze, directions newDirection) {
+
+		switch (DIRECTION) {
+		case NORTH:
+			if (TestStep(Maze) == true)
+				GetCOORD().second -= 1;
+			break;
+		case SOUTH:
+			if (TestStep(Maze) == true)
+				GetCOORD().second += 1;
+			break;
+		case WEST:
+			if (TestStep(Maze) == true)
+				GetCOORD().first -= 1;
+			break;
+		case EAST:
+			if (TestStep(Maze) == true)
+				GetCOORD().first += 1;
+			break;
+		}
+	}
 
 };
