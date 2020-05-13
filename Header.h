6dc8d622303743bb8ddf14bf45293c4a459aa995
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-14 00:50:36 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<windows.h>
+#include<iostream>
+#include<vector>
+#include<iterator>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<queue>
+#include<stack>
+#include<memory>
+#include<set>
+#include<cmath>
+#include<cstring>
+#include<cstdlib>
+#include <GL\glut.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+#define SQUARE_SIZE 20
+
+
+typedef pair<float, float> coordinates;
+
+
+
+
