6b131fdfe0443bcb8d0c4bed8756ecc074c89b6d
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-05-16 16:03:44 +0300

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -8,7 +8,7 @@
 void DrawMaze(HDC hdc, Table curMaze) {
 
 	float sideLength = 4;
-	HBRUSH hBrush; //создаём объект-кисть
+	HBRUSH hBrush; 
 	int quaterOfCell = SQUARE_SIZE / 4;
 	int partOfCell = SQUARE_SIZE / 2;
 
@@ -17,8 +17,8 @@ void DrawMaze(HDC hdc, Table curMaze) {
 
 			if (column.IsFreee()) {
 
-				hBrush = CreateSolidBrush(RGB(255, 0, 67)); //задаём сплошную кисть, закрашенную цветом RGB
-				SelectObject(hdc, hBrush); //делаем кисть активной
+				hBrush = CreateSolidBrush(RGB(255, 0, 67)); 
+				SelectObject(hdc, hBrush); 
 				Rectangle(hdc, column.GetCoord().second * SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE,
 					column.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
 				DeleteObject(hBrush);
@@ -26,16 +26,16 @@ void DrawMaze(HDC hdc, Table curMaze) {
 				switch (column.WhatBonus()) {
 
 				case REGULAR_POINT:
-					hBrush = CreateSolidBrush(RGB(193, 0, 32)); //задаём сплошную кисть, закрашенную цветом RGB
-					SelectObject(hdc, hBrush); //делаем кисть активной
+					hBrush = CreateSolidBrush(RGB(193, 0, 32)); 
+					SelectObject(hdc, hBrush); 
 					Ellipse(hdc, column.GetCoord().second * SQUARE_SIZE + quaterOfCell, column.GetCoord().first * SQUARE_SIZE + quaterOfCell,
 						column.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE - quaterOfCell, column.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE - quaterOfCell);
 					DeleteObject(hBrush);
 					break;
 
 				case ENERGIZER:
-					hBrush = CreateSolidBrush(RGB(0, 107, 60)); //задаём сплошную кисть, закрашенную цветом RGB
-					SelectObject(hdc, hBrush); //делаем кисть активной
+					hBrush = CreateSolidBrush(RGB(0, 107, 60)); 
+					SelectObject(hdc, hBrush); 
 					Ellipse(hdc, column.GetCoord().second * SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE,
 						column.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
 					DeleteObject(hBrush);
@@ -49,8 +49,8 @@ void DrawMaze(HDC hdc, Table curMaze) {
 			}
 
 			else {
-				hBrush = CreateSolidBrush(RGB(255, 255, 67)); //задаём сплошную кисть, закрашенную цветом RGB
-				SelectObject(hdc, hBrush); //делаем кисть активной
+				hBrush = CreateSolidBrush(RGB(255, 255, 67)); 
+				SelectObject(hdc, hBrush); 
 				Rectangle(hdc, column.GetCoord().second * SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE,
 					column.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, column.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
 				DeleteObject(hBrush);
@@ -64,16 +64,16 @@ void DrawMaze(HDC hdc, Table curMaze) {
 }
 
 void DrawPacMan(HDC hdc, PacMan pacman) {
-	HBRUSH hBrush; //создаём объект-кисть
-	hBrush = CreateSolidBrush(RGB(255, 255, 0)); //задаём сплошную кисть, закрашенную цветом RGB
+	HBRUSH hBrush; 
+	hBrush = CreateSolidBrush(RGB(255, 255, 0)); 
 	Ellipse(hdc, pacman.GetCoord().second * SQUARE_SIZE, pacman.GetCoord().first * SQUARE_SIZE,
 		pacman.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, pacman.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
 	DeleteObject(hBrush);
 }
 
 void DrawAliveGhost(HDC hdc, Level level) {
-	HBRUSH hBrush; //создаём объект-кисть
-	hBrush = CreateSolidBrush(RGB(139, 0, 255)); //задаём сплошную кисть, закрашенную цветом RGB
+	HBRUSH hBrush; 
+	hBrush = CreateSolidBrush(RGB(139, 0, 255)); 
 	for (auto ghost : level.GetAliveGhosts()) {
 		Ellipse(hdc, ghost.GetCoord().second * SQUARE_SIZE, ghost.GetCoord().first * SQUARE_SIZE,
 			ghost.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, ghost.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
@@ -82,8 +82,8 @@ void DrawAliveGhost(HDC hdc, Level level) {
 }
 
 void DrawDeadGhost(HDC hdc, Level level) {
-	HBRUSH hBrush; //создаём объект-кисть
-	hBrush = CreateSolidBrush(RGB(139, 0, 255)); //задаём сплошную кисть, закрашенную цветом RGB
+	HBRUSH hBrush; 
+	hBrush = CreateSolidBrush(RGB(139, 0, 255)); 
 	for (auto ghost : level.GetDeadGhosts()) {
 		Ellipse(hdc, ghost.GetCoord().second * SQUARE_SIZE, ghost.GetCoord().first * SQUARE_SIZE,
 			ghost.GetCoord().second * SQUARE_SIZE + SQUARE_SIZE, ghost.GetCoord().first * SQUARE_SIZE + SQUARE_SIZE);
@@ -109,9 +109,9 @@ void DrawLifes(HDC hdc, Level level) {
 
 void DrawScores(HDC hdc, PacMan pacman, Level level) {
 
-	char numOfLifes[50] = { '\0' };//можно ещё обнулить, для уверенности
+	char numOfLifes[50] = { '\0' };
 	char text[50] = "Scores: ";
-	_itoa_s(level.GetScores(), numOfLifes, 10);//переводим число в строку
+	_itoa_s(level.GetScores(), numOfLifes, 10);
 	strcat_s(text, numOfLifes);
 
 	TextOutA(hdc, 30.0 * SQUARE_SIZE, 2.0 * SQUARE_SIZE, text, strlen(text));
